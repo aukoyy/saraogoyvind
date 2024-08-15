64aad2904e7695c4b58ae4ea379f941c7ad844e9
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2024-08-15 08:25:23 +0200

    Make intro text and another carousel

--- src/app/bilder/page.tsx
+++ src/app/bilder/page.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client'
+import { Button, Carousel } from 'antd';
 import Image from "next/image";
-import { Carousel } from 'antd';
+import { HiMiniArrowTopRightOnSquare } from "react-icons/hi2";
 
 /* 
   4 seksjoner/karuseller
@@ -8,22 +10,36 @@ import { Carousel } from 'antd';
   2. Seremonien
   3. Fotoshoot
   4. Middag og fest
-*/
 
-const contentStyle: React.CSSProperties = {
-  margin: 0,
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
+  Shared album from LR: https://adobe.ly/3X5qiog
+  Remember to share a quick guide on how to download images from the shared album
+*/
 
 export default function ImageGallery() {
   return (
     <main className="min-h-screen mt-12 mx-2 flex justify-center">
-      <div className="my-24 max-w-screen-md w-full">
-        <Carousel arrows infinite={false}>
+      <div className="md:my-24 max-w-screen-md w-full space-y-16">
+        <div>
+          <h1 className="text-4xl font-bold text-center">Bildegalleri</h1>
+          <p className="mt-4 text-center">Velkommen til vårt bildegalleri. Her deler vi et utvalg bilder fra vårt bryllup.</p>
+          <p className="mt-4 text-center">
+            Om du ønsker å laste ned bilder, trykk på den eksterne lenken og finn de tre prikkene øverst til høyre og trykk "Last ned bilder". 
+            Det finnes en tilsvarende knapp i enkeltbildevisning.
+          </p>
+          <div className='flex justify-center'>
+            <Button 
+              // href='https://adobe.ly/3X5qiog' 
+              target='_blank' 
+              type='link' 
+              size="large" 
+              className="w-full md:w-1/2 font-semibold"
+            >
+              <p>Vis 372 bilder i eksternt album</p>
+              <HiMiniArrowTopRightOnSquare />
+            </Button>
+          </div>
+        </div>
+        <Carousel arrows infinite={false} adaptiveHeight draggable >
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
             src="/images/bryllup-preview.jpg"
@@ -41,16 +57,44 @@ export default function ImageGallery() {
             height={1080}
             priority
           />
-      </Carousel>
-
-      {/* <Image
+          <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
             src="/images/bryllup-preview1946.jpg"
             alt="Next.js Logo"
             width={1920}
             height={1080}
             priority
-          /> */}
+          />
+      </Carousel>
+      <Carousel arrows infinite={false} adaptiveHeight draggable >
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+          src="/images/bryllup-preview.jpg"
+          alt="Next.js Logo"
+          width={1920}
+          height={1080}
+          priority
+        />
+        
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+          src="/images/bryllup-preview0446.jpg"
+          alt="Next.js Logo"
+          width={1920}
+          height={1080}
+          priority
+        />
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+          src="/images/bryllup-preview1946.jpg"
+          alt="Next.js Logo"
+          width={1920}
+          height={1080}
+          priority
+        />
+    </Carousel>
+
+      
         
         
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen mt-12 mx-8">
-      <div className="mt-12 md:mt-24 flex justify-center">
+    <main className="min-h-screen mt-12 mx-8 flex justify-center">
+      <div className="mt-12 md:mt-24 text-center">
 
-        <p>Takketekst</p>
+        <h1>Work in progress</h1>
+        <p className="mt-4">Takketekst</p>
 
         {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"

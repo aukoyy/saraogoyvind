6dc1d790069f6110dea59d21335beac3641464fa
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2024-08-17 14:06:24 +0200

    Add image files and images to page

--- src/app/bilder/page.tsx
+++ src/app/bilder/page.tsx
@@ -15,10 +15,12 @@ import { HiMiniArrowTopRightOnSquare } from "react-icons/hi2";
   Remember to share a quick guide on how to download images from the shared album
 */
 
+
+
 export default function ImageGallery() {
   return (
     <main className="min-h-screen mt-12 mx-2 flex justify-center">
-      <div className="md:my-24 max-w-screen-md w-full space-y-16">
+      <div className="md:my-24 mb-96 max-w-screen-md w-full space-y-16">
         <div>
           <h1 className="text-4xl font-bold text-center">Bildegalleri</h1>
           <p className="mt-4 text-center">Velkommen til vårt bildegalleri. Her deler vi et utvalg bilder fra vårt bryllup.</p>
@@ -39,62 +41,283 @@ export default function ImageGallery() {
             </Button>
           </div>
         </div>
-        <Carousel arrows infinite={false} adaptiveHeight draggable >
+        <Carousel arrows infinite={false} adaptiveHeight draggable>
           <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-            src="/images/bryllup-preview.jpg"
-            alt="Next.js Logo"
-            width={1920}
-            height={1080}
-            priority
-          />
-          
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-            src="/images/bryllup-preview0446.jpg"
-            alt="Next.js Logo"
+            src="/images/instaeksport-01.jpg"
+            alt="Bilde av bryllup"
             width={1920}
             height={1080}
             priority
           />
           <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-            src="/images/bryllup-preview1946.jpg"
-            alt="Next.js Logo"
+            src="/images/instaeksport-02.jpg"
+            alt="Bilde av bryllup"
             width={1920}
             height={1080}
-            priority
+          />
+          <Image
+            src="/images/instaeksport-05.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
+          />
+          <Image
+            src="/images/instaeksport-06.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
+          />
+          <Image
+            src="/images/instaeksport-07.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
+          />
+          <Image
+            src="/images/instaeksport-08.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
+          />
+          <Image
+            src="/images/instaeksport-09.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
+          />
+          <Image
+            src="/images/instaeksport-10.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
+          />
+          <Image
+            src="/images/instaeksport-11.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
+          />
+          <Image
+            src="/images/instaeksport-12.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
+          />
+          <Image
+            src="/images/instaeksport-13.jpg"
+            alt="Bilde av bryllup"
+            width={1920}
+            height={1080}
           />
       </Carousel>
-      <Carousel arrows infinite={false} adaptiveHeight draggable >
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/images/bryllup-preview.jpg"
-          alt="Next.js Logo"
-          width={1920}
-          height={1080}
-          priority
-        />
-        
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/images/bryllup-preview0446.jpg"
-          alt="Next.js Logo"
-          width={1920}
-          height={1080}
-          priority
-        />
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/images/bryllup-preview1946.jpg"
-          alt="Next.js Logo"
-          width={1920}
-          height={1080}
-          priority
-        />
-    </Carousel>
 
-      
+      <div className='md:w-2/3 mx-auto'>
+        <Carousel arrows infinite={false} draggable>
+          <Image
+            src="/images/instaeksport-03.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+            priority
+          />
+          <Image
+            src="/images/instaeksport-04.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-14.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-15.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-16.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-17.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-19.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-20.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-21.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-22.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-23.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-24.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-25.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-27.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+        </Carousel>
+      </div>
+
+      <Carousel arrows infinite={false} adaptiveHeight draggable>
+        <Image
+          src="/images/instaeksport-18.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+          priority
+        />
+        <Image
+          src="/images/instaeksport-26.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-28.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-29.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-30.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-31.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-32.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-33.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-34.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-42.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+        <Image
+          src="/images/instaeksport-43.jpg"
+          alt="Bilde av bryllup"
+          width={1920}
+          height={1080}
+        />
+      </Carousel>
+
+      <div className='md:w-2/3 mx-auto'>
+        <Carousel adaptiveHeight arrows infinite={false} draggable>
+          <Image
+            src="/images/instaeksport-35.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+            priority
+          />
+          {/* <Image
+            src="/images/instaeksport-36.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          /> */}
+          <Image
+            src="/images/instaeksport-37.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-38.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-39.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-40.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+          <Image
+            src="/images/instaeksport-41.jpg"
+            alt="Bilde av bryllup"
+            width={1080}
+            height={1920}
+          />
+        </Carousel>
+      </div>
         
         
       </div>

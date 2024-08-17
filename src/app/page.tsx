165e79e7dcc905ce64089cd87919382ae3811996
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2024-08-17 14:28:53 +0200

    Legg til tekst

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import { thankYouParagraphs } from "./takketekst";
 
 export default function Home() {
   return (
-    <main className="min-h-screen mt-12 mx-8 flex justify-center">
-      <div className="mt-12 md:mt-24 text-center">
-
-        <h1>Work in progress</h1>
-        <p className="mt-4">Takketekst</p>
-
-        {/* <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/images/bryllup-preview.jpg"
+    <main className="min-h-screen md:my-12 mx-4 flex justify-center">
+      <div className="mt-12 md:mt-24 max-w-screen-md space-y-12">
+        <h1 className="text-4xl underline text-center">For en dag!</h1>
+        <div>  
+          {thankYouParagraphs.map((paragraph, index) => (
+            <p key={index} className="mt-4">
+              {paragraph}
+            </p>
+          ))}
+        </div>
+        <Image
+          src="/images/instaeksport-31.jpg"
           alt="Next.js Logo"
           width={1920}
           height={1080}
-          // sizes="(max-width: 768px) 100vw, 33vw"
           priority
-        /> */}
+        />
       </div>
 
       
